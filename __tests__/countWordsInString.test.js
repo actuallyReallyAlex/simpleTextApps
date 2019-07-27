83fb4f42fc11c2239f3dc8b330bf028526214138
alexlee-dev <alex@alexlee.dev>
2019-07-26 19:41:15 -0700

    ✨ Aadd countWordsInString().

--- __tests__/countWordsInString.test.js
+++ __tests__/countWordsInString.test.js
@@ -2,6 +2,14 @@ import { countWordsInString } from "../lib/index";
 
 describe("simpleTextApps.countWordsInString()", () => {
   it("Should count the number of words in the input string.", () => {
-    expect(countWordsInString("Alex is a good dev.")).toBe(5);
+    expect(countWordsInString("Alex is a good dev. Tom is a bad dev.")).toEqual({
+      a: 2,
+      Alex: 1,
+      bad: 1,
+      dev: 2,
+      good: 1,
+      is: 2,
+      Tom: 1
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -167,9 +167,25 @@ const checkIfPalindrome = (str: string) =>
     .reverse()
     .join("");
 
+const countWordsInString = (str: string) => {
+  const splitUp: Array<string> = str.replace(/\./g, "").split(" ");
+  const counts: { [index: string]: number } = {};
+
+  splitUp.forEach((word: string) => {
+    if (counts.hasOwnProperty(word)) {
+      counts[word]++;
+    } else {
+      counts[word] = 1;
+    }
+  });
+
+  return counts;
+};
+
 module.exports = {
   checkIfPalindrome,
   countVowels,
+  countWordsInString,
   pigLatin,
   stringReverse
 };
